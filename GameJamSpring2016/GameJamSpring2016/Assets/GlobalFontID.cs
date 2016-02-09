02e88be3738abe3afce89d3c0b3c34f5cffa793e
Singlecharacter <conorjpayton@gmail.com>
2016-02-09 14:48:43 -0500

    Added Visual Studio Ultraviolet Project

---
+++ GameJamSpring2016/GameJamSpring2016/Assets/GlobalFontID.cs
@@ -0,0 +1,12 @@
+using TwistedLogik.Ultraviolet.Content;
+
+namespace GameJamSpring2016.Assets
+{
+    /// <summary>
+    /// Contains the asset identifiers of the application's global font resources.
+    /// </summary>
+    public static class GlobalFontID
+    {
+        public static readonly AssetID SegoeUI;
+    }
+}
