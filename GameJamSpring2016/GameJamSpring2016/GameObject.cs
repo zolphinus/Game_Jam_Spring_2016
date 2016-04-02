8d587caa73c556eb6e313b66dd44e45a03b062ba
macbad2012 <cantleym01@mycu.concord.edu>
2016-04-02 11:26:27 -0400

    Game Object
    thing

---
+++ GameJamSpring2016/GameJamSpring2016/GameObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using GameJamSpring2016.Assets;
+using GameJamSpring2016.Input;
+using TwistedLogik.Nucleus;
+using TwistedLogik.Nucleus.Text;
+using TwistedLogik.Ultraviolet;
+using TwistedLogik.Ultraviolet.Content;
+using TwistedLogik.Ultraviolet.Graphics.Graphics2D;
+using TwistedLogik.Ultraviolet.Graphics.Graphics2D.Text;
+using TwistedLogik.Ultraviolet.OpenGL;
+using TwistedLogik.Ultraviolet.Platform;
+
+namespace SafeProjectName
+{
+    class GameObject
+    {
+        private Vector2 _position;
+        private Sprite _sprite;
+        private SpriteAnimationController[] _animations;
+        private Body _body2D;
+
+        public Vector2 position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public Sprite sprite
+        {
+            get { return _sprite; }
+            set { _sprite = value; }
+        }
+
+        public SpriteAnimationController[] animations
+        {
+            get { return _animations; }
+            set { _animations = value; }
+        }
+
+        public Body body2D
+        {
+            get { return _body2D; }
+            set { _body2D = value; }
+        }
+    }
+}
